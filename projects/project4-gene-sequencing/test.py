f9ff905436d08b2bcaef400bf5719d78a4bbfd42
Ty Klabacka <klabacka.ty@gmail.com>
2023-11-09 10:54:46 -0700

    added fill_matrix2

--- projects/project4-gene-sequencing/test.py
+++ projects/project4-gene-sequencing/test.py
@@ -8,8 +8,8 @@ MATCH = -3
 INDEL = 5
 SUB = 1
 
-word1 = "-other"
-word2 = "-thars"
+word1 = "-polynomial"
+word2 = "-exponential"
 
 class Node:
     def __init__(self, val, letter=None, parent=None, finished=0) -> None:
@@ -56,27 +56,27 @@ def fill_matrix(matrix, cur_row, cur_col):
             new_node.type = "delete"
             new_node.letter = "-"
             if add == 0:
-                new_node.value -= 3
+                new_node.value += MATCH
             else:
-                new_node.value += 5
+                new_node.value += INDEL
             
         elif min_val == upper_left:
             new_node.parent = upper_left
             new_node.type = "replace"
             new_node.letter = word1[cur_col]
-            if add == 0:
-                new_node.value -= 3
-            else:
-                new_node.value += 1
+            if add == 0 and not (cur_col == 0 and cur_row == 0):
+                new_node.value += MATCH
+            elif add == 1 and not (cur_col == 0 and cur_row == 0):
+                new_node.value += SUB
 
         elif min_val == left:
             new_node.parent = left
             new_node.type = "insert"
             new_node.letter = word1[cur_col]
             if add == 0:
-                new_node.value -= 3
+                new_node.value -= MATCH
             else:
-                new_node.value += 5
+                new_node.value += INDEL
 
 
     if cur_col != len(matrix[0]) - 1:
@@ -85,10 +85,30 @@ def fill_matrix(matrix, cur_row, cur_col):
     if cur_col == 0 and cur_row != len(matrix) - 1:
         fill_matrix(matrix, cur_row + 1, cur_col)
 
+def fill_matrix2(matrix):
+    for slot in range(len(matrix)):
+        matrix[slot][0] = slot
+    for slot in range(len(matrix[0])):
+        matrix[0][slot] = slot
+    for row in range(1, len(matrix)):
+        for col in range(1, len(matrix[0])):
+            matrix[row][col] = \
+            min((MATCH if word1[col] == word2[row] else SUB) + matrix[row - 1][col - 1],
+                INDEL + matrix[row][col - 1],
+                INDEL + matrix[row - 1][col]
+                )
+
+
 def printm(matrix):
     for row in matrix:
         for col in row:
             print((col.value, col.type[:1]), end=" ")
+        print()
+
+def printm2(matrix):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            print(matrix[row][col], end=" ")
         print()
 
 def modify_string(matrix):
@@ -97,23 +117,29 @@ def modify_string(matrix):
     cur_idx = len(mod_string) - 1
     start = matrix[len(word2) - 1][len(word1) - 1]
 
-
-    while start.parent.letter != None and cur_idx >= 0:
+    cur_indel = 0
+    while start.parent.letter != None:
+        if start.type == "delete" or start.type == "insert":
+            cur_indel += 1
+        if cur_indel < MAXINDELS:
+            print("TOO MANY INDELS")
+            return
         mod_string[cur_idx] = start.letter
         start = start.parent
-        cur_idx -= 1
     
     print(mod_string)
-
 
 
 matrix = [[None for itm in word1] for itm in word2]
 
 
-fill_matrix(matrix, 0, 0)
+# fill_matrix(matrix, 0, 0)
 
-modify_string(matrix)
 
-printm(matrix)
+# modify_string(matrix)
+
+
+fill_matrix2(matrix)
+printm2(matrix)
 
 
